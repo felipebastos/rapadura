37384647c374d0cb25501f7f6e93c3dadf03d659
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2023-09-15 18:12:52 -0300

    Roteando a tela inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { Pagina1Component } from './pagina1/pagina1.component';
+import { Pagina2Component } from './pagina2/pagina2.component';
+
+const routes: Routes = [
+  { path: 'um', component: Pagina1Component },
+  { path: 'dois', component: Pagina2Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { PokelistaComponent } from './pokelista/pokelista.component';
 import { ListaService } from './pokelista/lista.service';
 import { HttpClientModule } from '@angular/common/http';
+import { Pagina1Component } from './pagina1/pagina1.component';
+import { Pagina2Component } from './pagina2/pagina2.component';
 
 @NgModule({
-  declarations: [AppComponent, PokelistaComponent],
+  declarations: [AppComponent, PokelistaComponent, Pagina1Component, Pagina2Component],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [ListaService],
   bootstrap: [AppComponent],
